1ee68dd75209487f11015393c31d6623c45ef0e9
Jamie Socorro <jamie_socorro@live.com>
2024-05-28 15:08:56 +0800

    feat: initial text editor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, input, model, signal } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'avnon';
+
+  openModal = signal<boolean>(false);
+
+  titleHeader = signal<string>('');
+  description = signal<string>('');
+
+  onTextChange(e: any) {
+    this.titleHeader.set(e.target.value);
+  }
+
+  onDescriptionChange(e: any) {
+    this.description.set(e.target.textContent);
+  }
+
+  onOpenModal() {
+    this.openModal.set(true);
+  }
+
+  onCloseModal() {
+    this.openModal.set(false);
+  }
+
+  onPublishMarkdown() {
+    this.onOpenModal();
+  }
 }
